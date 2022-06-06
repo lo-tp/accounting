a8f83d8834f3eb1119e360f1161c2f54b5945c37
lo-tp <regesteraccount@hotmail.com>
2022-06-06 17:29:47 -0600

    feat: write a mongodb relevant demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import clientPromise from '../lib/mongodb'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ isConnected }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -51,6 +52,15 @@ const Home: NextPage = () => {
             </p>
           </a>
         </div>
+                {isConnected ? (
+          <h2 className="subtitle">You are connected to MongoDB</h2>
+        ) : (
+          <h2 className="subtitle">
+            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
+            for instructions.
+          </h2>
+        )}
+
       </main>
 
       <footer className={styles.footer}>
@@ -70,3 +80,25 @@ const Home: NextPage = () => {
 }
 
 export default Home
+export async function getServerSideProps(context) {
+  try {
+    await clientPromise
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+    //
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+
+    return {
+      props: { isConnected: true },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
+}
